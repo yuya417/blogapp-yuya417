4143a7bf39b45c1a54c8bd90155009598f2cf04e
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-11-10 12:36:30 +0900

    follow機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,12 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
+
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender,to: :profile, allow_nil: true
@@ -45,6 +51,15 @@ class User < ApplicationRecord
     #   self.email.split('@').first
     # end
     profile&.nickname || self.email.split('@').first
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
   end
 
   def prepare_profile
